25a37faf34ca8defa0d0cf6c8f9582c3f0654156
Guilherme Mariano de Oliveira <guilherme_mariano95@hotmail.com>
2024-08-30 21:22:20 -0300

    adicionar imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
   reactStrictMode: true,
-};
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), 'styles')]
+  }
+}
 
 export default nextConfig;
