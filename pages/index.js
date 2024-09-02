e0d9e2d2d66b91bd899f499990c99d9889a3eb82
Guilherme Mariano de Oliveira <guilherme_mariano95@hotmail.com>
2024-09-02 11:43:05 -0300

    avatares e botao

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Botao from "@/componentes/botao";
+
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <h1>Ola Mundo!</h1>
+    <>
+      <h1>Ola Mundo!</h1>
+      <div style={{width: 200}}>
+        
+      <Botao texto={'Login'} manipularClique={() => console.log('botao clicado')} />
+        </div>
+    </>
   )
-  }
+}
