79a9b87acf4d796de188e8c364e8c3b55b7f0800
Guilherme Mariano de Oliveira <guilherme_mariano95@hotmail.com>
2024-09-23 21:04:39 -0300

    componentes de botao, avatar, uploadImagens

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,29 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Botao from "@/componentes/botao";
+import { UploadImagem } from "@/componentes/uploadImagem";
+import { useRef, useState } from "react";
 
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [imagem, setImagem] = useState(null);
+  const referenciaInput = useRef(null);
   return (
     <>
       <h1>Ola Mundo!</h1>
+      <button onClick={() => referenciaInput?.current?.click()}>abrir seletor de arquvos</button>
+      
+      <UploadImagem 
+      setImagem={setImagem}
+      imagemPreview={imagem?.preview}
+      aoSetarAReferencia={(ref) => referenciaInput.current = ref}
+       />
       <div style={{width: 200}}>
-        
-      <Botao texto={'Login'} manipularClique={() => console.log('botao clicado')} />
+        <Avatar />    
+      <Botao texto={'Login'} cor='invertido' manipularClique={() => console.log('botao clicado')} />
         </div>
     </>
   )
